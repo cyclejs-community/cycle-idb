b65daf5b2baa4d769e0f7bf4a9f36a8f27eddc37
Avalander <darth.quesorico@gmail.com>
2017-07-12 11:05:01 +0200

    Added usage example

---
+++ examples/hello-world/main.js
@@ -0,0 +1,44 @@
+import xs from 'xstream'
+
+import { run } from '@cycle/run'
+import { makeDOMDriver, div, ul, li } from '@cycle/dom'
+
+import makeIdbDriver, { $put } from 'cycle-idb'
+
+
+const PONIES = [
+	{ id: 2, name: 'Pinkie Pie', type: 'earth pony' },
+	{ id: 3, name: 'Applejack', type: 'earth pony' },
+	{ id: 4, name: 'Rainbow Dasy', type: 'pegasus' },
+	{ id: 5, name: 'Rarity', type: 'unicorn' },
+	{ id: 6, name: 'Fluttershy', type: 'pegasus' },
+]
+
+function main(sources) {
+	const addPonies$ = xs.periodic(1000).take(5)
+		.map(x => PONIES[x])
+		.map(x => $put('ponies', x))
+	
+	const vtree$ = sources.IDB.store('ponies').getAll()
+		.map(ponies => ul(
+			ponies.map(pony => li(`${pony.name} is a ${pony.type}.`))
+		))
+	
+	return {
+		IDB: addPonies$,
+		DOM: vtree$,
+	}
+}
+
+const drivers = {
+	IDB: makeIdbDriver('pony-db', 1, upgradeDb => {
+		switch (upgradeDb.oldVersion) {
+			case 0:
+				const store = upgradeDb.createObjectStore('ponies', { keyPath: 'id' })
+				store.put({ id: 1, name: 'Twilight Sparkle', type: 'unicorn' })
+		}	
+	}),
+	DOM: makeDOMDriver('#app'),
+}
+
+run(main, drivers)
