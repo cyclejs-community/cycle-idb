b495910a870e5b38124050a91e2d6a1296717366
Avalander <darth.quesorico@gmail.com>
2017-07-17 22:13:40 +0200

    Refactored Store.js to use streams better

--- src/Store.js
+++ src/Store.js
@@ -23,92 +23,110 @@ function IndexSelector(dbPromise, result$, storeName, indexName) {
 	const filterByKey = key => ({ result }) => (key === undefined || result.indexes[indexName].oldValue === key || result.indexes[indexName].newValue === key)
 
 	return {
-		getAll: MultiKeyCache(key => {
-			const filteredResult$ = result$.filter(filterByKey(key))
-			const options = { dbPromise, storeName, indexName, key, operation: 'getAll' }
-			return DbSelector(filteredResult$, ReadDbIndexListener, options)
-		}),
 		get: MultiKeyCache(key => {
-			const filteredResult$ = result$.filter(filterByKey(key))
-			const options = { dbPromise, storeName, indexName, key, operation: 'get' }
-			return DbSelector(filteredResult$, ReadDbIndexListener, options)
+			const readFromDb = ReadFromDbIndex({ dbPromise, storeName, indexName, key, operation: 'get' })
+			const dbResult$$ = result$.filter(filterByKey(key))
+				.startWith(1)
+				.map(readFromDb)
+				.map(promiseToStream)
+			const dbResult$ = flattenConcurrently(dbResult$$)
+			return adapt(dbResult$)
 		}),
-		count: MultiKeyCache(key => {
-			const filteredResult$ = result$.filter(filterByKey(key))
-			const options = { dbPromise, storeName, indexName, key, operation: 'count' }
-			return DbSelector(filteredResult$, ReadDbIndexListener, options)
-				.compose(dropRepeats())
+		getAll: MultiKeyCache(key => {
+			const readFromDb = ReadFromDbIndex({ dbPromise, storeName, indexName, key, operation: 'getAll' })
+			const dbResult$$ = result$.filter(filterByKey(key))
+				.startWith(1)
+				.map(readFromDb)
+				.map(promiseToStream)
+			const dbResult$ = flattenConcurrently(dbResult$$)
+			return adapt(dbResult$)
 		}),
 		getAllKeys: MultiKeyCache(key => {
-			const filteredResult$ = result$
+			const readFromDb = ReadFromDbIndex({ dbPromise, storeName, indexName, key, operation: 'getAllKeys' })
+			const dbResult$$ = result$.filter(filterByKey(key))
 				.filter(({ result }) => result.indexes.hasOwnProperty(indexName))
 				.filter(({ result }) => xor(result.indexes[indexName].oldValue !== key, result.indexes[indexName].newValue !== key))
-				.filter(filterByKey(key))
-			const options = { dbPromise, storeName, indexName, key, operation: 'getAllKeys' }
-			return DbSelector(filteredResult$, ReadDbIndexListener, options)
-		})
+				.startWith(1)
+				.map(readFromDb)
+				.map(promiseToStream)
+			const dbResult$ = flattenConcurrently(dbResult$$)
+			return adapt(dbResult$)
+		}),
+		count: MultiKeyCache(key => {
+			const readFromDb = ReadFromDbIndex({ dbPromise, storeName, indexName, key, operation: 'count' })
+			const dbResult$$ = result$.filter(filterByKey(key))
+				.startWith(1)
+				.map(readFromDb)
+				.map(promiseToStream)
+			const dbResult$ = flattenConcurrently(dbResult$$).compose(dropRepeats())
+			return adapt(dbResult$)
+		}),
 	}
 }
 
 const xor = (a, b) => (a || b) && !(a && b)
 
-function DbSelector(result$, Listener, options) {
-	return adapt(xs.createWithMemory({
-		start: listener => result$
-			.startWith(1)
-			.addListener(Listener(listener, options)),
-		stop: () => {},
-	}))
-}
-
 const resultIsInsertedOrDeleted = ({ result }) => result.operation === 'inserted' || result.operation === 'deleted'
 
-const GetSelector = (dbPromise, result$, storeName, key) => 
-	DbSelector(result$.filter(({ result }) => result.key === key), ReadDbListener, {
-		dbPromise, storeName, operation: 'get', key
-	})
+const GetSelector = (dbPromise, result$, storeName, key) => {
+	const readFromDb = ReadFromDb({ dbPromise, storeName, operation: 'get', key })
+	const dbResult$$ = result$.filter(({ result }) => result.key === key)
+		.startWith(1)
+		.map(readFromDb)
+		.map(promiseToStream)
+	const dbResult$ = flattenConcurrently(dbResult$$)
+	return adapt(dbResult$)
+}
 
-const GetAllSelector = (dbPromise, result$, storeName) => 
-	DbSelector(result$, ReadDbListener, { dbPromise, storeName, operation: 'getAll' })
+const GetAllSelector = (dbPromise, result$, storeName) => {
+	const readFromDb = ReadFromDb({ dbPromise, storeName, operation: 'getAll' })
+	const dbResult$$ = result$.startWith(1)
+		.map(readFromDb)
+		.map(promiseToStream)
+	const dbResult$ = flattenConcurrently(dbResult$$)
+	return adapt(dbResult$)
+}
 
-const CountSelector = (dbPromise, result$, storeName) =>
-	DbSelector(result$, ReadDbListener, { dbPromise, storeName, operation: 'count'})
-		.compose(dropRepeats())
+const CountSelector = (dbPromise, result$, storeName) => {
+	const readFromDb = ReadFromDb({ dbPromise, storeName, operation: 'count' })
+	const dbResult$$ = result$.startWith(1)
+		.map(readFromDb)
+		.map(promiseToStream)
+	const dbResult$ = flattenConcurrently(dbResult$$).compose(dropRepeats())
+	return adapt(dbResult$)
+}
 
 const GetAllKeysSelector = (dbPromise, result$, storeName) => {
-	const filteredResult$ = result$.filter(resultIsInsertedOrDeleted)
-	return DbSelector(filteredResult$, ReadDbListener, { dbPromise, storeName, operation: 'getAllKeys' })
+	const readFromDb = ReadFromDb({ dbPromise, storeName, operation: 'getAllKeys' })
+	const dbResult$$ = result$.filter(resultIsInsertedOrDeleted)
+		.startWith(1)
+		.map(readFromDb)
+		.map(promiseToStream)
+	const dbResult$ = flattenConcurrently(dbResult$$)
+	return adapt(dbResult$)
 }
 
-function ReadDbListener(listener, { dbPromise, storeName, operation, key }) {
-	return {
-		next: async value => {
-			try {
-				const db = await dbPromise
-				const data = await db.transaction(storeName)
-					.objectStore(storeName)[operation](key)
-				listener.next(data)
-			} catch (e) {
-				listener.error(e)
-			}
-		},
-		error: e => listener.error(e)
-	}
+const ReadFromDb = ({ dbPromise, storeName, operation, key }) => async () => {
+	const db = await dbPromise
+	const data = await db.transaction(storeName)
+		.objectStore(storeName)[operation](key)
+	return data
 }
 
-function ReadDbIndexListener(listener, { dbPromise, storeName, indexName, operation, key }) {
-	return {
-		next: async value => {
-			try {
-				const db = await dbPromise
-				const data = await db.transaction(storeName)
-					.objectStore(storeName)
-					.index(indexName)[operation](key)
-				listener.next(data)
-			} catch (e) {
-				listener.error(e)
-			}
-		},
-		error: e => listener.error(e)
-	}
+const ReadFromDbIndex = ({ dbPromise, storeName, indexName, operation, key}) => async () => {
+	const db = await dbPromise
+	const data = await db.transaction(storeName)
+		.objectStore(storeName)
+		.index(indexName)[operation](key)
+	return data
+}
+
+function promiseToStream(p) {
+	const $ = xs.fromPromise(p)
+	$.addListener({
+		next: () => {},
+		error: () => {},
+		complete: () => {},
+	})
+	return $
 }
