89e191db095a9e94b1b650c6b13b443de632bb53
Avalander <darth.quesorico@gmail.com>
2017-07-18 18:43:58 +0200

    Splitted tests by api

---
+++ src/__test__/idb-driver.store-api.test.js
@@ -0,0 +1,298 @@
+import test from 'tape'
+import sinon from 'sinon'
+
+import xs from 'xstream'
+import fromDiagram from 'xstream/extra/fromDiagram'
+
+import { 
+	mockIdb,
+	mockDatabase,
+	mockDbWithIndex,
+	mockDbWithTypeIndex,
+	mockDbWithNameIndex,
+	mockDbWithNumberIndex,
+} from './idb-driver.mock'
+import idb from 'idb'
+
+import {
+	getTestId,
+	sequenceListener,
+} from './test'
+
+import makeIdbDriver, {
+	$add,
+	$delete,
+	$put,
+	$update,
+} from '../idb-driver'
+
+
+test('store(...).getAll() should send db updates to listeners', t => {
+	t.plan(1)
+
+	const driver = makeIdbDriver(getTestId(), 1, mockDatabase())(xs.of(
+		$put('ponies', { name: 'Twilight Sparkle', type: 'unicorn' })
+	))
+	driver.store('ponies').getAll().drop(1)
+		.addListener({
+			next: value => {
+				t.deepEqual(value, [{ name: 'Twilight Sparkle', type: 'unicorn' }])
+				t.end()
+			}
+		})
+})
+
+test('store(...).getAll() should send multiple db updates to listeners', t => {
+	t.plan(4)
+
+	const driver = makeIdbDriver(getTestId(), 1, mockDatabase())(
+		fromDiagram('-a-b-c--|', {
+			values: {
+				a: $put('ponies',  { name: 'Twilight Sparkle', type: 'unicorn' }),
+				b: $put('ponies', { name: 'Rainbow Dash', type: 'pegasus' }),
+				c: $put('ponies', { name: 'Twilight Sparkle', type: 'alicorn' }),
+			},
+			timeUnit: 20,
+		})
+	)
+	driver.store('ponies').getAll().addListener(sequenceListener(t)([
+		value => t.deepEqual(value, []),
+		value => t.deepEqual(value, [{ name: 'Twilight Sparkle', type: 'unicorn' }]),
+		value => t.deepEqual(value, [
+			{ name: 'Rainbow Dash', type: 'pegasus' },
+			{ name: 'Twilight Sparkle', type: 'unicorn' },
+		]),
+		value => t.deepEqual(value, [
+			{ name: 'Rainbow Dash', type: 'pegasus' },
+			{ name: 'Twilight Sparkle', type: 'alicorn' },
+		]),
+	]))
+})
+
+test('store(...).getAll() should send value in db when a listener is registered', t => {
+	t.plan(1)
+
+	const driver = makeIdbDriver(getTestId(), 1, mockDatabase([
+		{ name: 'Twilight Sparkle', type: 'unicorn' }
+	]))(xs.empty())
+	driver.store('ponies').getAll()
+		.addListener({
+			next: value => {
+				t.deepEqual(value, [{ name: 'Twilight Sparkle', type: 'unicorn' }])
+				t.end()
+			}
+		})
+})
+
+test('store(...).getAll() should send empty list when a listener is registered to an empty store', t => {
+	t.plan(1)
+
+	const driver = makeIdbDriver(getTestId(), 1, mockDatabase())(xs.empty())
+	driver.store('ponies').getAll()
+		.addListener({
+			next: value => {
+				t.deepEqual(value, [])
+				t.end()
+			}
+		})
+})
+
+test('store(...).get() selector should work', t => {
+	t.plan(3)
+
+	const driver = makeIdbDriver(getTestId(), 1, mockDatabase())(fromDiagram('-a-b-c--|', {
+		values: {
+			a: $put('ponies', { name: 'Rainbow Dash', type: 'pegasus' }),
+			b: $put('ponies', { name: 'Twilight Sparkle', type: 'unicorn' }),
+			c: $update('ponies', { name: 'Rainbow Dash', type: 'pegasus', colour: 'blue' }),
+		},
+		timeUnit: 20,
+	}))
+	driver.store('ponies').get('Rainbow Dash')
+		.addListener(sequenceListener(t)([
+			value => t.equal(value, undefined, 'First value should be undefined'),
+			value => t.deepEqual(value, { name: 'Rainbow Dash', type: 'pegasus' }, 'Second value should be Rainbow Dash'),
+			value => t.deepEqual(value, { name: 'Rainbow Dash', type: 'pegasus', colour: 'blue' }, 'Third value should be updated Rainbow Dash'),
+		]))
+})
+
+test('store(...).get() selector should send undefined value when no key found', t => {
+	t.plan(1)
+
+	const driver = makeIdbDriver(getTestId(), 1, mockDatabase())(xs.never())
+	driver.store('ponies').get('Pinkie Pie')
+		.addListener({
+			next: value => t.equal(value, undefined)
+		})
+})
+
+test('store(...).get() selector shound send db value when listener registers', t => {
+	t.plan(1)
+
+	const driver = makeIdbDriver(getTestId(), 1, mockDatabase([
+		{ name: 'Pinkie Pie', type: 'Earth Pony' },
+		{ name: 'Rainbow Dash', type: 'Pegasus' },
+	]))(xs.never())
+	driver.store('ponies').get('Pinkie Pie')
+		.addListener({
+			next: value => t.deepEqual(value, { name: 'Pinkie Pie', type: 'Earth Pony' })
+		})
+})
+
+test('store(...).get() selector should send undefined when value is deleted', t => {
+	t.plan(2)
+
+	const driver = makeIdbDriver(getTestId(), 1, mockDatabase([
+		{ name: 'Rarity', type: 'unicorn' }
+	]))(fromDiagram('-a--|', {
+		values: {
+			a: $delete('ponies', 'Rarity'),
+		},
+		timeUnit: 20,
+	}))
+	driver.store('ponies').get('Rarity')
+		.addListener(sequenceListener(t)([
+			value => t.deepEqual(value, { name: 'Rarity', type: 'unicorn' }, 'Rarity is in database'),
+			value => t.equal(value, undefined, 'When Rarity is removed, undefined is sent'),
+		]))
+})
+
+test('store(...).count() should start with 0 when store is empty', t => {
+	t.plan(1)
+
+	const driver = makeIdbDriver(getTestId(), 1, mockDatabase())(xs.never())
+	driver.store('ponies').count()
+		.addListener(sequenceListener(t)([
+			value => t.equal(value, 0),
+		]))
+})
+
+test('store(...).count() should start with 1 when store has one element', t => {
+	t.plan(1)
+
+	const driver = makeIdbDriver(getTestId(), 1, mockDatabase([
+		{ name: 'Twilight Sparkle', type: 'unicorn' },
+	]))(xs.never())
+	driver.store('ponies').count()
+		.addListener(sequenceListener(t)([
+			value => t.equal(value, 1),
+		]))
+})
+
+test('store(...).count() should send new count when an element is added', t=> {
+	t.plan(2)
+
+	const driver = makeIdbDriver(getTestId(), 1, mockDatabase())(
+		fromDiagram('-a--|', {
+			values: {
+				a: $put('ponies', { name: 'Twilight Sparkle', type: 'unicorn' }),
+			},
+			timeUnit: 20,
+		}))
+	driver.store('ponies').count()
+		.addListener(sequenceListener(t)([
+			value => t.equal(value, 0),
+			value => t.equal(value, 1),
+		]))
+})
+
+test('store(...).count() should send new count when an element is removed', t => {
+	t.plan(2)
+
+	const driver = makeIdbDriver(getTestId(), 1, mockDatabase([
+		{ name: 'Fluttershy', type: 'pegasus' },
+	]))(fromDiagram('-a--|', {
+		values: {
+			a: $delete('ponies', 'Fluttershy'),
+		},
+		timeUnit: 20,
+	}))
+	driver.store('ponies').count()
+		.addListener(sequenceListener(t)([
+			value => t.equal(value, 1),
+			value => t.equal(value, 0),
+		]))
+})
+
+test('store(...).count() should only broadcast when count changes', t => {
+	t.plan(2)
+
+	const driver = makeIdbDriver(getTestId(), 1, mockDatabase([
+		{ name: 'Fluttershy', type: 'pegasus' },
+	]))(fromDiagram('-a-b--|', {
+		values: {
+			a: $put('ponies', { name: 'Fluttershy', type: 'pegasus', element: 'kindness' }),
+			b: $put('ponies', { name: 'Applejack', type: 'earth pony' }),
+		},
+		timeUnit: 20,
+	}))
+
+	driver.store('ponies').count().addListener(sequenceListener(t)([
+		value => t.equal(value, 1),
+		value => t.equal(value, 2),
+		value => t.fail('Too many events'),
+	]))
+})
+
+test('store(...).getAllKeys() should return all keys present in the store', t => {
+	t.plan(1)
+
+	const driver = makeIdbDriver(getTestId(), 1, mockDatabase([
+		{ name: 'Fluttershy', type: 'pegasus' },
+		{ name: 'Pinkie Pie', type: 'earth pony' },
+		{ name: 'Rarity', type: 'unicorn' },
+	]))(xs.never())
+	driver.store('ponies').getAllKeys()
+		.addListener({
+			next: value => t.deepEqual(value, [ 'Fluttershy', 'Pinkie Pie', 'Rarity' ])
+		})
+})
+
+test('store(...).getAllKeys() should get updates when a new object is added to the store', t => {
+	t.plan(2)
+
+	const driver = makeIdbDriver(getTestId(), 1, mockDatabase([
+		{ name: 'Fluttershy', type: 'pegasus' },
+		{ name: 'Pinkie Pie', type: 'earth pony' },
+	]))(xs.of(
+		$put('ponies', { name: 'Rarity', type: 'unicorn' })
+	))
+	driver.store('ponies').getAllKeys()
+		.addListener(sequenceListener(t)([
+			value => t.deepEqual(value, [ 'Fluttershy', 'Pinkie Pie' ], 'Fluttershy and Pinkie are in the list'),
+			value => t.deepEqual(value, [ 'Fluttershy', 'Pinkie Pie', 'Rarity' ], 'Rarity is added')
+		]))
+})
+
+test('store(...).getAllKeys() should get updates when an object is removed from the store', t => {
+	t.plan(2)
+
+	const driver = makeIdbDriver(getTestId(), 1, mockDatabase([
+		{ name: 'Fluttershy', type: 'pegasus' },
+		{ name: 'Pinkie Pie', type: 'earth pony' },
+	]))(xs.of(
+		$delete('ponies', 'Pinkie Pie')
+	))
+	driver.store('ponies').getAllKeys()
+		.addListener(sequenceListener(t)([
+			value => t.deepEqual(value, [ 'Fluttershy', 'Pinkie Pie' ], 'Fluttershy and Pinkie are in the list'),
+			value => t.deepEqual(value, [ 'Fluttershy' ], 'Pinkie is removed')
+		]))
+})
+
+test('store(...).getAllKeys() should not get updates when an existing object is modified', t => {
+	t.plan(1)
+
+	const driver = makeIdbDriver(getTestId(), 1, mockDatabase([
+		{ name: 'Twilight Sparkle', type: 'unicorn' },
+		{ name: 'Pinkie Pie', type: 'earth pony' },
+		{ name: 'Rainbow Dash', type: 'pegasus' },
+	]))(xs.of(
+		$update('ponies', { name: 'Twilight Sparkle', type: 'alicorn' })
+	))
+	driver.store('ponies').getAllKeys()
+		.addListener(sequenceListener(t)([
+			value => t.deepEqual(value, [ 'Pinkie Pie', 'Rainbow Dash', 'Twilight Sparkle' ], 'Twilight, Rainbow and Pinkie are in the list'),
+			value => t.fail(`Unexpected value: ${JSON.stringify(value)}`),
+		]))
+})
